b0fe746dcadbccfd0321838e0962a19b9a9947ac
GOB Tech <88345156+gobstech@users.noreply.github.com>
2022-12-31 19:27:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function darkMode() {
 	var main = document.querySelector(".main");
-	var icon = document.querySelector(".icon");
 	var btnsLinks = document.querySelector(".btn-default");
 
 	main.classList.toggle("active");
-	icon.classList.toggle("active");
 	btnsLinks.forEach((btn) => {
 		btn.classList.toggle("active");
 	});
